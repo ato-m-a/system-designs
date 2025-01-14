824968bb14a90564604ffd4a4626d5a77906f056
Tom <99224451+ato-m-a@users.noreply.github.com>
2025-01-14 20:53:01 +0900

    Initial Commit

---
+++ packages/shared/src/index.tsx
@@ -0,0 +1,8 @@
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+  return <button onClick={onClick}>{label}</button>;
+};
